5ac64a1a6e33951908bb8bb8cbca0d0b16d09d34
79003 <dimangrigorgan@yandex.tu>
2024-04-04 22:59:06 +0300

    сделали регистрацию

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect
-
+from data.users import User
+from data.login_form import LoginForm
 from data.reg_form import RegForm
+from flask import Flask, render_template, redirect
+from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bforg-site_secret_key'
+db_session.global_init('db/db_ads.db')
 
 
 @app.route('/')
@@ -16,12 +19,30 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
     form = RegForm()
-    if form.validate_on_submit():
+    db_sess = db_session.create_session()
+    users = db_sess.query(User).all()
+    if form.validate_on_submit() and form.email.data in [user.email for user in users]:
+        return render_template('registration.html', title='Зарегистрироваться',
+                               form=form, message='Существующая почта')
+    if form.validate_on_submit() and form.password.data != form.password_2.data:
+        return render_template('registration.html', title='Зарегистрироваться',
+                               form=form, message='разные пароли')
+    if form.validate_on_submit() and form.password.data == form.password_2.data:
+        db_sess = db_session.create_session()
+        user = User()
+        user.nickname = form.nickname.data
+        user.age = form.age.data
+        user.email = form.email.data
+        user.hashed_password = form.password.data
+        user.set_password(user.hashed_password)
+        db_sess.add(user)
+        db_sess.commit()
         return redirect('/login')
-    return render_template('registration.html', form=form)
+    return render_template('registration.html', title='Зарегистрироваться',
+                           form=form)
 
 
 if __name__ == '__main__':
