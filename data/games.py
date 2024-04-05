5070096cdac3a861d0db1702464fae0df9466c8b
79003 <dimangrigorgan@yandex.tu>
2024-04-05 21:29:01 +0300

    update

--- data/games.py
+++ data/games.py
@@ -4,13 +4,11 @@ from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
 
 
-class Ads(SqlAlchemyBase):
-    __tablename__ = 'ads'
+class Games(SqlAlchemyBase):
+    __tablename__ = 'Games'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    id_user = sqlalchemy.Column(sqlalchemy.Integer,
-                                    sqlalchemy.ForeignKey("user.id"))
     id_game = sqlalchemy.Column(sqlalchemy.Integer)
     text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     product_quantity = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
@@ -18,4 +16,3 @@ class Ads(SqlAlchemyBase):
                                    default=datetime.datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                  default=datetime.datetime.now)
-    user = orm.relationship('User')
